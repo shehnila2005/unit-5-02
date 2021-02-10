cb982ca47facd7832a159310bef945d7d382e4e9
Shehnila Azmat <shehnila.azmat@ocsbstudent.ca>
2021-02-10 22:52:21 +0000

    spaces

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function buttonOn() {
+function buttonOn () {
   document.getElementById("paragraph").innerHTML = "Done"
 }
 
-function  pineappleButton() {
-  document.getElementById("picture").src                   = "pineapple.jpg"
-  document.getElementById("picture").alt                   = "pineaaple"
-  document.getElementById("background").style.backgroundcolor = "red"
+function pineappleButton () {
+  document.getElementById("picture").src = "pineapple.jpg" 
+  document.getElementById("picture").alt  = "pineaaple"
+  document.getElementById("background") .style.backgroundcolor = "red"
 }
