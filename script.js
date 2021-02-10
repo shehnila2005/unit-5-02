a40a57bb4477736d6ee36f0ccd18c8aa736dc054
Shehnila Azmat <shehnila.azmat@ocsbstudent.ca>
2021-02-10 23:08:35 +0000

    trailing spaces

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function buttonOn () {
 }
 
 function pineappleButton () {
-  document.getElementById("picture").src="pineapple.jpg" 
-  document.getElementById("picture").alt="pineaaple"
+  document.getElementById("picture").src= "pineapple.jpg"
+  document.getElementById("picture").alt= "pineaaple"
   document.getElementById("background").style.backgroundcolor = "red"
 }
