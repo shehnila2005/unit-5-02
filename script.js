727dcaba9298d1952c84957fdb5679803a986278
Shehnila Azmat <shehnila.azmat@ocsbstudent.ca>
2021-02-10 21:16:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function buttonOn() {
+  document.getElementById("paragraph").innerHTML = "Done"
+}
+
+function pineappleButton() {
+document.getElementById("picture"). src ="pineapple.jpg"
+document.getElementById("picture").alt = "pineaaple"
+document.getElementById("background").style.backgroundcolor = "red"
+}
